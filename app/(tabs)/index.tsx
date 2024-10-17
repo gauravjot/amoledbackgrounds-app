9ae6cc0cc7220a690e77c84cef9c9f32eb03573c
Gauravjot Garaya <gauravjot@outlook.com>
2024-10-16 21:51:55 -0700

    added components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,17 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { View } from 'react-native';
+import { Text } from '@/components/ui/Text';
+import { Button, ButtonText } from '@/components/ui/Button';
+import { Input } from '@/components/ui/Input';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <View style={styles.titleContainer}>
+      <View>
         <Text>Welcome!</Text>
+        <Text>Some Text</Text>
+        <Button variant={"accent"}>
+          <ButtonText>Click me!</ButtonText>
+        </Button>
+        <Input/>
       </View>
-      <View style={styles.stepContainer}>
-        <Text>Step 1: Try it</Text>
-        <Text>
-          Edit <Text>app/(tabs)/index.tsx</Text> to see changes.
-          Press{' '}
-          <Text>
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </Text>{' '}
-          to open developer tools.
-        </Text>
-      </View>
-      <View style={styles.stepContainer}>
-        <Text>Step 2: Explore</Text>
-        <Text>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </Text>
-      </View>
-      <View style={styles.stepContainer}>
-        <Text>Step 3: Get a fresh start</Text>
-        <Text>
-          When you're ready, run{' '}
-          <Text>npm run reset-project</Text> to get a fresh{' '}
-          <Text>app</Text> directory. This will move the current{' '}
-          <Text>app</Text> to{' '}
-          <Text>app-example</Text>.
-        </Text>
-      </View>
-    </ParallaxScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
---
+++ components/ui/Text.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils/cn";
+import * as React from "react";
+import { Text as RNText } from "react-native";
+
+const Text = React.forwardRef<
+	React.ElementRef<typeof RNText>,
+	React.ComponentPropsWithoutRef<typeof RNText>
+>(({ className, ...props }, ref) => {
+	return (
+		<RNText
+			className={cn("text-base text-foreground", className)}
+			ref={ref}
+			{...props}
+		/>
+	);
+});
+Text.displayName = "Text";
+
+export { Text };
