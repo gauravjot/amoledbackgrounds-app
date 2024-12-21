ba3abe1443a6eca41b6dc6d9583d6e45ed180f49
Gauravjot Garaya <gauravjotg@gmail.com>
2024-12-21 15:35:33 -0800

    updated changelog

--- components/ChangeLog.tsx
+++ components/ChangeLog.tsx
@@ -3,10 +3,10 @@ import {Text} from "./ui/Text";
 import {Button, ButtonText} from "./ui/Button";
 import React from "react";
 import Animated, {FadeInUp, FadeOutDown} from "react-native-reanimated";
-import {PencilRuler, Send, Settings, Sparkles} from "lucide-react-native";
+import {Send, Sparkles} from "lucide-react-native";
 
 export default function ChangeLogDialog({isVisible, onClose}: {isVisible: boolean; onClose: () => void}) {
-  const CHANGELOG_TITLE = "🎉 V2.0.0 is here!";
+  const CHANGELOG_TITLE = "🎉 V2.1.0 is here!";
   const width = Dimensions.get("window").width;
   const height = Dimensions.get("window").height;
 
@@ -26,39 +26,14 @@ export default function ChangeLogDialog({isVisible, onClose}: {isVisible: boolea
         <Text className="p-4 text-[1.5rem] font-bold">{CHANGELOG_TITLE}</Text>
         <ScrollView className="px-4">
           <Text className={style.body_text}>
-            AmoledBackgrounds Update – Major Rewrite!
-            {"\n\n"}Thank You for using AmoledBackgrounds. This version brings major changes to the app, including —
+            Thank You for using AmoledBackgrounds. This version brings following changes to the app —
           </Text>
-          <View className={style.body_header}>
-            <PencilRuler size="18" color="#df83a2" fill="#785765" />
-            <Text className={style.body_header_text}>Complete Redesign</Text>
-          </View>
-          <Text className={style.body_text}>{`•   Brand-new UI for a cleaner, modern look.`}</Text>
           <View className={style.body_header}>
             <Sparkles size="18" color="#ffcd0a" fill="#bfa41e" />
             <Text className={style.body_header_text}>NEW</Text>
           </View>
-          <Text className={style.body_text}>{`•   Daily Wallpaper: "Downloaded" as an option.`}</Text>
-          <Text className={style.body_text}>{`•   View downloaded wallpapers post on Reddit.`}</Text>
-          <Text className={style.body_text}>{`•   Delete downloaded wallpapers.`}</Text>
-          <Text
-            className={
-              style.body_text
-            }>{`•   Carousel Viewer: Swipe through downloaded wallpapers effortlessly.`}</Text>
-          <Text
-            className={
-              style.body_text
-            }>{`•   Download Progress: See percentage progress while downloading wallpapers.`}</Text>
-          <View className={style.body_header}>
-            <Settings size="18" color="#03a9f4" fill="#2c4d72" />
-            <Text className={style.body_header_text}>Improved Settings</Text>
-          </View>
-          <Text className={style.body_text}>{`•   Save sort preferences and search history.`}</Text>
-          <Text className={style.body_text}>{`•   Send error logs to help improve the app.`}</Text>
-          <Text className={style.body_text}>{`•   Check required permissions easily.`}</Text>
-          <Text className={style.body_text}>
-            {`\nLet us know what you think of the new update by leaving a review on the Play Store, or reach out to us directly.`}
-          </Text>
+          <Text className={style.body_text}>{`•   Black Pixel Percentage. Thanks to AmoledBot on subreddit.`}</Text>
+          <Text className={style.body_text}>{`•   Support for gallery posts.`}</Text>
           <View className={style.body_header}>
             <Send size="18" color="white" />
             <Text className={style.body_header_text}>Contact Developer</Text>
